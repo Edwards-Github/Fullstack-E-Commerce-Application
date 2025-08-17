dcbb52b95b4f66279ecd5cf7eef6b6de1c9c59fc
Edwards-Github <84117648+Edwards-Github@users.noreply.github.com>
2025-08-16 18:01:44 -0700

    initial commit

---
+++ section3/eazystore-ui/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header.jsx';
+import ClassHeader from './components/ClassHeader';
+import Footer from './components/footer/Footer';
+import React from 'react';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Home />
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ section3/eazystore-ui/src/components/PageTitle.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function PageTitle(props) {
+  return <h1 className="page-title">{props.title}</h1>;
+}
