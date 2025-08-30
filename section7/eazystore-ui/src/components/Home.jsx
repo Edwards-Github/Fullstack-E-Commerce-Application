0fe8f47db76fc0b6b0f854ac9ee12f667bf60a9d
Edwards-Github <84117648+Edwards-Github@users.noreply.github.com>
2025-08-30 16:58:02 -0700

    Connecting to java spring backend

---
+++ section7/eazystore-ui/src/components/Home.jsx
@@ -0,0 +1,29 @@
+import PageHeading from './PageHeading';
+import ProductListings from './ProductListings';
+import apiClient from '../api/apiClient';
+import { useState, useEffect } from 'react';
+
+export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  // Run once when the component mounts
+  // Mounting is the process of creating and adding the component into DOM
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    const response = await apiClient.get('/products'); // Axios GET Request
+    setProducts(response.data); // Update product state with fetched data
+  };
+
+  return (
+    <div className="max-w-[1152px] mx-auto px-6 py-8">
+      <PageHeading title="Explore Eazy Stickers!">
+        Add a touch of creativity to your space with our wide range of fun and
+        unique stickers. Perfect for any occasion!
+      </PageHeading>
+      <ProductListings products={products} />
+    </div>
+  );
+}
