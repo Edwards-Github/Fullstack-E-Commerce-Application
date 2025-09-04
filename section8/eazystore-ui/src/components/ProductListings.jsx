58e27430fbc16b075a971963e2bf9378d13facbc
Edwards-Github <84117648+Edwards-Github@users.noreply.github.com>
2025-09-04 16:42:00 -0700

    - Added search and sort functionality to website

--- section8/eazystore-ui/src/components/ProductListings.jsx
+++ section8/eazystore-ui/src/components/ProductListings.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductCard from './ProductCard';
 import SearchBox from './SearchBox';
 import Dropdown from './Dropdown';
@@ -6,13 +6,44 @@ import Dropdown from './Dropdown';
 const sortList = ['Popularity', 'Price Low to High', 'Price High to Low'];
 
 export default function ProductListings({ products }) {
-  let searchText = '';
+  const [searchText, setSearchText] = useState('');
+  const [selectedSort, setSelectedSort] = useState('Popularity');
 
   function handleSearchChange(inputSearch) {
-    searchText = inputSearch;
-    console.log(inputSearch);
+    setSearchText(inputSearch);
   }
 
+  function handleSortChange(sortType) {
+    setSelectedSort(sortType);
+  }
+
+  let filteredAndSortedProducts = Array.isArray(products)
+    ? products.filter(
+        (product) =>
+          product.name.toLowerCase().includes(searchText.toLowerCase()) ||
+          product.description.toLowerCase().includes(searchText.toLowerCase())
+      )
+    : [];
+
+  switch (selectedSort) {
+    case 'Price Low to High':
+      filteredAndSortedProducts = filteredAndSortedProducts.sort(
+        (a, b) => parseFloat(a.price) - parseFloat(b.price)
+      );
+      break;
+    case 'Price High to Low':
+      filteredAndSortedProducts = filteredAndSortedProducts.sort(
+        (a, b) => parseFloat(b.price) - parseFloat(a.price)
+      );
+      break;
+    case 'Popularity':
+      break;
+    default:
+      filteredAndSortedProducts = filteredAndSortedProducts.sort(
+        (a, b) => parseInt(b.popularity) - parseInt(a.popularity)
+      );
+      break;
+  }
   return (
     <div className="max-w-[1152px] mx-auto">
       <div className="flex flex-col sm:flex-row justify-between items-center gap-4 pt-12">
@@ -22,11 +53,16 @@ export default function ProductListings({ products }) {
           value={searchText}
           handleSearch={(value) => handleSearchChange(value)}
         />
-        <Dropdown label="Sort by" options={sortList} value="Popularity" />
+        <Dropdown
+          label="Sort by"
+          options={sortList}
+          value={selectedSort}
+          handleSort={(value) => handleSortChange(value)}
+        />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-12">
-        {products.length > 0 ? (
-          products.map((product) => (
+        {filteredAndSortedProducts.length > 0 ? (
+          filteredAndSortedProducts.map((product) => (
             <ProductCard key={product.productId} product={product} />
           ))
         ) : (
