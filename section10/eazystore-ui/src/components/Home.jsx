eadcff9d5a52efd9394955f1c237d09c455be4f8
Edwards-Github <84117648+Edwards-Github@users.noreply.github.com>
2025-09-18 12:46:58 -0700

    - started loader functions

--- section10/eazystore-ui/src/components/Home.jsx
+++ section10/eazystore-ui/src/components/Home.jsx
@@ -4,47 +4,6 @@ import apiClient from '../api/apiClient';
 import { useState, useEffect } from 'react';
 
 export default function Home() {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  // Run once when the component mounts
-  // Mounting is the process of creating and adding the component into DOM
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-
-  const fetchProducts = async () => {
-    try {
-      setLoading(true);
-      const response = await apiClient.get('/products'); // Axios GET Request
-      setProducts(response.data); // Update product state with fetched data
-    } catch (error) {
-      setError(
-        error.response?.data?.message ||
-          'Failed to fetch products. Please try again.'
-      ); // Extract error message if available
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  if (loading) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <span className="text-xl font-semibold">Loading products...</span>
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <span className="text-xl text-red-500">Error: {error}</span>
-      </div>
-    );
-  }
-
   return (
     <div className="max-w-[1152px] mx-auto px-6 py-8">
       <PageHeading title="Explore Eazy Stickers!">
@@ -54,4 +13,11 @@ export default function Home() {
       <ProductListings products={products} />
     </div>
   );
+}
+
+export async function productsLoader() {
+  try {
+    const response = await apiClient.get('/products'); // Axios GET Request
+    return response.data;
+  } catch (error) {}
 }
