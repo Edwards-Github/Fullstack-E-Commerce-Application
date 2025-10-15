09148bab1f73be752df630eba95a35113d889108
Edwards-Github <84117648+Edwards-Github@users.noreply.github.com>
2025-10-15 16:31:12 -0700

    October 15, 2025 1. Updated "Contact.java" to auto generate id 2. Information submitted in the contact page will go to the database

--- section9/eazystore/src/main/java/com/eazybytes/eazystore/service/impl/ContactServiceImpl.java
+++ section9/eazystore/src/main/java/com/eazybytes/eazystore/service/impl/ContactServiceImpl.java
@@ -1,19 +1,14 @@
 package com.eazybytes.eazystore.service.impl;
 
 import com.eazybytes.eazystore.dto.ContactRequestDto;
-import com.eazybytes.eazystore.dto.ProductDto;
 import com.eazybytes.eazystore.entity.Contact;
-import com.eazybytes.eazystore.entity.Product;
 import com.eazybytes.eazystore.repository.ContactRepository;
 import com.eazybytes.eazystore.service.IContactService;
-import com.eazybytes.eazystore.service.IProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
