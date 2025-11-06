a26126d832f42b625648e9f686ae476af44976dd
Edwards-Github <84117648+Edwards-Github@users.noreply.github.com>
2025-11-06 12:00:03 -0800

    November 6, 2025 - Created ProductDetail.jsx - Shows product description in the Product Detail page

--- section10/eazystore-ui/src/components/ProductDetail.jsx
+++ section10/eazystore-ui/src/components/ProductDetail.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 
 export default function ProductDetail() {
   const params = useParams();
-
-  return <div>{params.productId}</div>;
+  const location = useLocation();
+  const product = location.state?.product;
+  return <div>{product.description}</div>;
 }
